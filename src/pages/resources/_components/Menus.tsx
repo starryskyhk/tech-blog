0b35e704176af4395626cf4782f81cd4b1e52a30
kun.han <kun.han@thoughtworks.com>
2024-01-30 19:39:47 +0800

    customiza the Resource Menus

--- src/pages/resources/_components/Menus.tsx
+++ src/pages/resources/_components/Menus.tsx
@@ -1,25 +1,25 @@
-import React, { useState } from 'react';
-import { Menu } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { ConfigProvider, Menu } from 'antd';
 import clsx from 'clsx'
 import { resources } from "@site/data/resources"
 import styles from '../resource.module.css'
 import Link from "@docusaurus/Link";
 
-
-const linkComponent = <Link className={styles.resourceCardLink} />;
+const linkComponent = <Link className={styles.resourceCardLink}/>;
 
 
 function generateMenuItem(name) {
   return (
-    <Menu.Item key={name} >
-      {React.cloneElement(linkComponent, { href: `#${name}` })}
+    <Menu.Item key={name}>
+      {React.cloneElement(linkComponent, {href: `#${name}`})}
       {name}
     </Menu.Item>
   )
 }
 
 function generateSubMenu({items, name}) {
-  return <Menu.SubMenu key={name} children={generateMenu(items)} title={<span>{name}</span>}/>
+  return <Menu.SubMenu key={name} children={generateMenu(items)}
+                       title={<span>{name}</span>}/>
 }
 
 function generateMenu(items) {
@@ -33,14 +33,26 @@ const Menus: React.FC = () => {
   const [collapsed] = useState(false);
 
   return (
-    <nav className={clsx(styles.sidebar, 'thin-scrollbar')}>
-      <div style={{width: 256}}>
-        <Menu mode="inline" inlineCollapsed={collapsed}>
-          {menus}
-        </Menu>
-      </div>
-    </nav>
+    <ConfigProvider
+      theme={{
+        components: {
+          Menu: {
+            darkItemBg: "#1b1b1d",
+            darkSubMenuItemBg: "#242526"
+          },
+        },
+      }}
+    >
+      <nav className={clsx(styles.sidebar, 'thin-scrollbar')}>
+        <div style={{width: 256}}>
+          <Menu mode="inline" inlineCollapsed={collapsed}>
+            {menus}
+          </Menu>
+        </div>
+      </nav>
+    </ConfigProvider>
   );
 };
+
 
 export default Menus;
