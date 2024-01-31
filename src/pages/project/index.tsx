c8987d8e7b945cb37a74c8e69a451a6ef3607e90
kun.han <kun.han@thoughtworks.com>
2024-01-31 21:23:01 +0800

    modify name

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -17,10 +17,9 @@ const TITLE = translate({
 })
 const DESCRIPTION = translate({
   id: 'theme.project.description',
-  message: '学而无用，不如学而用之。这里是我在技术领域中努力实践和应用的最佳证明。',
+  message: '学而无用，不如学而用之',
 })
 
-// const GITHUB_URL = 'https://github.com/kuizuo'
 
 type ProjectState = {
   scrollTopPosition: number
